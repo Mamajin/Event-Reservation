2492f4b59cb8ae82792d8eca3ad42d5f6f63bc6d
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-10 22:17:46 +0700

    Add constraint into max_attendee attribute

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 
 class Organizer(models.Model):
@@ -44,7 +45,7 @@ class Event(models.Model):
     start_date_register = models.DateTimeField('Registration Start Date', default=timezone.now)
     end_date_register = models.DateTimeField('Registration End Date', null=False, blank=False)
     description = models.TextField(max_length=400)
-    max_attendee = models.IntegerField(default=0)
+    max_attendee = models.IntegerField(default=0, validators = [MinValueValidator(0)])
 
 
     
