c95ef1af7257a7a8ac1b1ff6bbd8b2b7fee464c6
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-10-10 14:51:58 +0700

    Create Organizer model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Organizer(models.Model):
+    """
+    An Organizer model class contains organizer attributes
+    and a method...
+    """
+    organizer_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    event_list = models.ManyToManyField(Event, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self):
+        return self.user.first_name
+    
