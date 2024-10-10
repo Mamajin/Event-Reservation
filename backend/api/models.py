50b01af6390cd19e6b06752c955d6a501146f13e
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-10-10 16:22:55 +0700

    Create method for Organizer and Ticket model and docstring for every models

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,40 +4,127 @@ from django.utils import timezone
 
 
 class Event(models.Model):
+    """
+    Represents an event with a name, creation date, start/end dates, 
+    registration period, and a description.
+
+    Attributes:
+        event_name (str): The name of the event.
+        event_create_date (datetime): The date and time the event was created.
+        start_date_event (datetime): The date and time the event starts.
+        end_date_event (datetime): The date and time the event ends.
+        start_date_register (datetime): The start date for registration.
+        end_date_register (datetime): The end date for registration.
+        description (str): A brief description of the event (up to 400 characters).
+    """
     event_name = models.CharField(max_length=100)
-    event_create_date = models.DateTimeField('Event Created At', default= timezone.now)
-    start_date_event = models.DateTimeField('Event Start Date', null = False, blank= False)
-    end_date_event = models.DateTimeField('Event End Date', null= False, blank = True)
+    event_create_date = models.DateTimeField('Event Created At', default=timezone.now)
+    start_date_event = models.DateTimeField('Event Start Date', null=False, blank=False)
+    end_date_event = models.DateTimeField('Event End Date', null=False, blank=True)
     start_date_register = models.DateTimeField('Registration Start Date', default=timezone.now)
-    end_date_register = models.DateTimeField('Registration End Date', null = False , blank= False)
+    end_date_register = models.DateTimeField('Registration End Date', null=False, blank=False)
     description = models.TextField(max_length=400)
-    
-    
-    
+
+    def __str__(self):
+        """
+        Return a string representation of the Event object.
+        
+        Returns:
+            str: The name of the event.
+        """
+        return self.event_name
+
+
 class Attendee(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    event_list = models.ManyToManyField(Event, through= 'Ticket')
-    
-    
-# Create your models here.
+    """
+    Represents an attendee (user) who can attend multiple events.
+
+    Attributes:
+        user (User): A foreign key referencing the Django User model.
+        event_list (ManyToManyField): A list of events the attendee has registered for,
+                                      represented via the Ticket model.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_list = models.ManyToManyField(Event, through='Ticket')
+
+    def __str__(self):
+        """
+        Return a string representation of the Attendee object.
+        
+        Returns:
+            str: The first name of the associated User.
+        """
+        return self.user.first_name
+
+
 class Organizer(models.Model):
     """
-    An Organizer model class contains organizer attributes
-    and a method...
+    Represents an organizer who can organize multiple events.
+
+    Attributes:
+        organizer_name (str): The name of the organizer.
+        email (str): The email address of the organizer.
+        event_list (ManyToManyField): A list of events organized by this organizer.
     """
     organizer_name = models.CharField(max_length=100)
     email = models.EmailField()
     event_list = models.ManyToManyField(Event, related_name='events')
 
     def __str__(self):
-        return self.user.first_name
+        """
+        Return a string representation of the Organizer object.
+        
+        Returns:
+            str: The name of the organizer.
+        """
+        return self.organizer_name
     
-    
-    
-class Ticket(models.Model):
-    event = models.ForeignKey(Event, on_delete= models.CASCADE)
-    attendee = models.ForeignKey(Attendee, on_delete= models.CASCADE)
-    register_date = models.DateTimeField('Date registered', default= timezone.now)
+    def add_event(self, event):
+        """
+        Adds an event to the organizer's event list.
 
+        Args:
+            event (Event): The event to be added.
+        """
+        self.event_list.add(event)
+
+    def remove_event(self, event):
+        """
+        Removes an event from the organizer's event list.
+
+        Args:
+            event (Event): The event to be removed.
+        """
+        self.event_list.remove(event)
+
+
+
+class Ticket(models.Model):
+    """
+    Represents a ticket that connects an Attendee to an Event. 
+    Tracks the date of registration for the event.
+
+    Attributes:
+        event (ForeignKey): The event the ticket is for.
+        attendee (ForeignKey): The attendee who registered for the event.
+        register_date (datetime): The date the attendee registered for the event.
+    """
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attendee = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+    register_date = models.DateTimeField('Date registered', default=timezone.now)
+
+    def __str__(self):
+        """
+        Return a string representation of the Ticket object.
+        
+        Returns:
+            str: A string indicating the event name for the ticket.
+        """
+        return f"Ticket for {self.event.event_name}"
     
-    
+    def cancel_ticket(self):
+        """
+        Cancels the ticket by deleting the Ticket instance.
+        """
+        self.delete()
+
