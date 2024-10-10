8955c6a7aebb68b89b46cb52c9e8217cbe8955ce
Phasit Ruangmak <phasit2005@gmail.com>
2024-10-10 15:14:39 +0700

    Added Registration form and empty placeholder pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import react from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import ProtectedRoute from "./components/ProtectedRoute"
+import react from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
-function Logout() {
+function Logout(){
   localStorage.clear()
   return <Navigate to="/login" />
 }
 
-function RegisterAndLogout() {
+function RegisterAndLogout(){
   localStorage.clear()
   return <Register />
 }
@@ -24,16 +24,17 @@ function App() {
           path="/"
           element={
             <ProtectedRoute>
-              <Home />
+              <Home/>
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/login" element={<Logout />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
+    
   )
 }
 
