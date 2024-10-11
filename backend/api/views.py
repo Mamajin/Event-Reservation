ebff8a11916a4b164da99e5abf96aabf05a36839
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-10-11 21:38:16 +0700

    Create CreateOrganizerView in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from api.serializers import UserSerializer, OrganizerSerializer, EventSerializer, AttendeeSerializer, TicketSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from api.models import Organizer, Event, Attendee, Ticket
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class CreateOrganizerView(generics.CreateAPIView):
+    queryset = Organizer.objects.all()
+    serializer_class = OrganizerSerializer
+    permission_classes = [IsAuthenticated]
     
