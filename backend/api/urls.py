d58b33e1c964f328145dcfc9a5488ac77135e154
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-14 10:30:09 +0700

    Implement UserAPI and change from DRF to api_endpoint in views.py

---
+++ backend/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from api.views import api
+
+urlpatterns = [
+    path('', api.urls)
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,16 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 import stub_api.urls
+import api.urls
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth", include("rest_framework.urls")),
-    path("stub_api/", include(stub_api.urls))
+    path("stub_api/", include(stub_api.urls)),
+    path("", include(api.urls)),
 ]
