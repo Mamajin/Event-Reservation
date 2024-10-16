4b9b674d8ded4e484722157108a3b3a0a68c3512
Sunthorn Kompita <sunthorn.ko@ku.th>
2024-10-17 02:10:36 +0700

    Initialize CreateEventForm component with basic structure

--- frontend/src/components/CreateEventForm.jsx
+++ frontend/src/components/CreateEventForm.jsx
@@ -1,4 +1,21 @@
+import React, { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import qs from 'qs';
+
 function CreateEventForm() {
+
+    const [formData, setFormData] = useState({
+        event_name: "",
+        start_date_register: "",
+        end_date_register: "",
+        start_date_event: "",
+        end_date_event: "",
+        description: "",
+        max_attendee: "",
+    });
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 }
 
 export default CreateEventForm;
