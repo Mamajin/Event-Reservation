b2d595b3d41a9876bb43059a90278b32e675dc38
Sunthorn Kompita <sunthorn.ko@ku.th>
2024-10-23 03:35:43 +0700

    Add state management for Form Data

--- frontend/src/components/OrganizerForm.jsx
+++ frontend/src/components/OrganizerForm.jsx
@@ -1,4 +1,16 @@
-function OrganizerForm(){
+import React, { useState } from 'react';
 
+function OrganizerForm() {
+    const [formData, setFormData] = useState({
+        organizer_name: '',
+        email: '',
+    });
+
+    return (
+        <div>
+            <h1>Organizer Form</h1>
+        </div>
+    );
 }
-export default OrganizerForm
+
+export default OrganizerForm;
