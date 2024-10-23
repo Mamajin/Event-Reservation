3e796a8773cf1a655c43db8fa7ddcc7ea684b498
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-10-23 18:18:29 +0700

    Added router for sessions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from ninja import NinjaAPI
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
 from api.views.user import router as user_router
 from api.views.organizer import router as organizer_router
 from api.views.event import router as event_router
 from api.views.ticket import router as ticket_router
+from api.views.session import router as session_router
 
 api = NinjaExtraAPI(version ="2.0.0")
 api.register_controllers(NinjaJWTDefaultController)
@@ -15,6 +15,7 @@ api.add_router("/users/", user_router)
 api.add_router("/organizers/", organizer_router)
 api.add_router("/events/", event_router)
 api.add_router("/tickets/", ticket_router)
+api.add_router("/sessions/", session_router)
 
 urlpatterns = [
     path("", api.urls),  # Prefix all API routes with /api/
