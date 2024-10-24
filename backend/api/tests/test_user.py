334cf7c7e602d84752f0cd63055f7ec1b3d14469
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-24 10:05:50 +0700

    Add test that user can get profile and add function that can get_token of user

--- backend/api/tests/test_user.py
+++ backend/api/tests/test_user.py
@@ -1,23 +1,37 @@
 from api.urls import api  
 from api.models import AttendeeUser
-from .utlis import BaseModelsTest
+from .utlis import BaseModelsTest, RefreshToken, AccessToken
 
 class UserAPITests(BaseModelsTest):
+    
+    def get_token_for_user(self, user):
+        """Helper method to generate a JWT token for the test user"""
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+            
+        
     def test_user_creation(self):
         response = self.client.post(self.user_create_url, data={
             'username': 'attendeeuser1',
             'first_name': 'Jane',
             'last_name': 'Doe',
-            'birth_date': '1995-06-15',  # Correctly formatted date string
+            'birth_date': '1995-06-15',
             'phone_number': '9876543210',
             'password': 'password123',
             'password2': 'password123',
-            'email': "jane@example.com"# Ensure this matches password
+            'email': "jane@example.com"
         })
         self.assertEqual(response.status_code, 200)
         user = AttendeeUser.objects.get(username = 'attendeeuser1')
         self.assertTrue(AttendeeUser.objects.filter(username = user.username).exists())
         
         
-    
+    def test_get_profile(self):
+        token = self.get_token_for_user(self.test_user)
+        response = self.client.get(self.user_profile_url, 
+                                   headers={"Authorization": f"Bearer {token}"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['username'], self.test_user.username)
+        self.assertIn('status', response.json())
+        
         
