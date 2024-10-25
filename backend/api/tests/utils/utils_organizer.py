c8e62653257461b89bc7d8673086bc8286b36912
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-25 17:25:38 +0700

    Add tests for organizer and create blank test_ticket file

---
+++ backend/api/tests/utils/utils_organizer.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.utils import timezone
+from api.models import AttendeeUser, Organizer, Event, Ticket
+from datetime import datetime
+from ninja.testing import TestClient
+from api.urls import organizer_router
+from ninja_jwt.tokens import RefreshToken
+from faker import Faker
+from django.contrib.auth import get_user_model
+import datetime
+
+fake = Faker()
+
+class OrganizerModelsTest(TestCase):
+
+    def setUp(self):
+        """
+        Set up initial test data for models.
+        """
+        self.event_id  = None
+        self.client = TestClient(organizer_router)
+        self.apply_organizer_url = '/apply-organizer'
+        self.delete_event_url = f"/delete-event/"
+        
+        self.test_user = AttendeeUser.objects.create_user(
+            username='attendeeuser3',
+            password='password123',
+            first_name='Jane',
+            last_name='Doe',
+            birth_date='1995-06-15',
+            phone_number='9876543210',
+            email='jane.doe@example.com'
+        )
+        
+        self.event_test = Event.objects.create(
+            event_name=fake.company(),
+            organizer= self.become_organizer(self.test_user, "test_user"),
+            start_date_event=timezone.now(),
+            end_date_event= timezone.now() + datetime.timedelta(days = 1),  # Ensure it ends after it starts
+            start_date_register=timezone.now() - datetime.timedelta(days = 2),  # Example for registration start
+            end_date_register=timezone.now() + datetime.timedelta(days = 3),  # Registration ends when the event starts
+            max_attendee=fake.random_int(min=10, max=500),
+            description=fake.text(max_nb_chars=200)
+        )
+        
+    def get_token_for_user(self, user):
+        """Helper method to generate a JWT token for the test user"""
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+    
+    
+    def become_organizer(self,user, organizer_name):
+        self.organizer, created = Organizer.objects.get_or_create(
+            user= user,
+            organizer_name = organizer_name,
+        )
+        return self.organizer
+
+    
+    def create_user(self, username, first_name):
+        user = AttendeeUser.objects.create(
+            username = username, 
+            first_name = first_name,
+            last_name = 'Doe',
+            birth_date='1995-06-15',
+            phone_number='9876543210',
+            email='jane.doe@example.com'
+        )
+        user.set_password("password123")
+        return  user
+    
+    
+
+        
+            
+        
