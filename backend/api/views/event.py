f5bb314805e49f44c5cf9bc7e8458faff924d764
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-26 16:38:34 +0700

    Add date condition to create event

--- backend/api/views/event.py
+++ backend/api/views/event.py
@@ -29,9 +29,12 @@ class EventAPI:
             )
         except Exception as e:
             raise HttpError(status_code=400, message=f"Failed to create event: {str(e)}")
+        if event.is_valid_date():
+            return event        
+        else:
+            return Response({'error': 'Please enter valid date'}, status = 400)
+            
         
-        # Return the created event
-        return event
         
     
     @router.get('/my-events', response=List[EventResponseSchema], auth=JWTAuth())
