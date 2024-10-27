8a8f851361ea813cec628b0efbf3b67ed0f8aaa4
Sunthorn KOMPITA <sunthorn.ko@ku.th>
2024-10-28 05:25:29 +0700

    Set localStorage after attendee become organizer

--- frontend/src/components/OrganizerForm.jsx
+++ frontend/src/components/OrganizerForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import api from '../api';
 import { useNavigate } from 'react-router-dom';
-import { ACCESS_TOKEN } from '../constants';
+import { ACCESS_TOKEN, USER_STATUS } from '../constants';
 
 function OrganizerForm() {
     const [formData, setFormData] = useState({
@@ -39,6 +39,7 @@ function OrganizerForm() {
 
             alert(errorMessage);
         } finally {
+            localStorage.setItem(USER_STATUS, "Organizer");
             setLoading(false);
         }
     };
