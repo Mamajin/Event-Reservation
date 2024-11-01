151440bbb2271dca93880c7ff490c8331d398739
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-11-02 02:58:44 +0700

    Added all the AWS Key and Secret ID of IAM user.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,22 @@ GOOGLE_MAPS_API_KEY = config("GOOGLE_MAPS_API_KEY")
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='ap-southeast-1')  # e.g., 'ap-southeast-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='ap-southeast-1') 
+
+# S3 configuration
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
+AWS_DEFAULT_ACL = 'public-read' 
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = False 
+
+# Storage configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# URLs configuration
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Application definition
 
@@ -159,15 +170,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-MEDIA_URL = '/media/'
-MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
