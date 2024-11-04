f226b44ceea73503f8b66621254dceaef2ab7481
Sunthorn Kompita <sunthorn.ko@ku.th>
2024-11-04 20:42:37 +0700

    add new constant to collect the user profile

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,3 +2,4 @@ export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
 export const USER_NAME = "username";
 export const USER_STATUS = "status"
+export const PROFILE_PICTURE = "picture"
