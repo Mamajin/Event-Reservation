b782ff5cdf8f26a88d26a8d566a4c455d16ee323
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-11-05 17:04:19 +0700

    Deleted unnecessary Install apps.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,7 +97,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'corsheaders.middleware.CorsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
