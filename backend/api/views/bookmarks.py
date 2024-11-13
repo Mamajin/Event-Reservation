6c15c3f9ec87a14a90c5cb005dc0fa6fb2f015dd
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-11-14 00:51:02 +0700

    Add test for new bookmark views

--- backend/api/views/bookmarks.py
+++ backend/api/views/bookmarks.py
@@ -28,50 +28,6 @@ class BookmarkAPI:
         return event_data
     
     
-    @router.post('/{event_id}/', auth=JWTAuth())
-    def create_bookmark(request : HttpRequest, event_id : int):
-        """
-        Creates a new bookmark for a specific event if it does not already exist.
-
-        Args:
-            request (HttpRequest): The HTTP request object containing the authenticated user.
-            event_id (int): The ID of the event to be bookmarked.
-
-        Returns:
-            Response: A success message if the bookmark is created, or an error message if the event is already bookmarked.
-        """
-        event = Event.objects.get(id = event_id)
-
-        if Bookmarks.objects.filter(attendee = request.user, event = event).exists():
-            return Response({'error': 'This event is already in your favorites'}, status = 400)
-        
-        user = request.user
-
-        Bookmarks.objects.create(event = event, attendee = user)
-
-        return Response({'success': 'This event has been added to your favorites.'}, status = 200)
-    
-    
-    @router.delete('/{event_id}/remove', auth=JWTAuth())
-    def delete_bookmark(request: HttpRequest, event_id: int):
-        """
-        Delete a bookmark for a specific event if the user has permission.
-
-        Args:
-            request (HttpRequest): HTTP request with authenticated user.
-            event_id (int): ID of the event to remove from bookmarks.
-
-        Returns:
-            Response: Success message if the bookmark is deleted, 
-                      error message if the user doesn't have permission.
-        """
-        event = Event.objects.get(id=event_id)        
-        if Bookmarks.objects.filter(attendee=request.user, event=event).exists():
-            bookmark = Bookmarks.objects.get(attendee=request.user, event=event)
-            bookmark.delete()            
-            return Response({'success': 'This event has been removed from your favorites'}, status=200)        
-        return Response({'error': 'You do not have permission to delete this'}, status=404)
-    
     @router.put('/{event_id}/toggle-bookmark', auth=JWTAuth())
     def toggle_bookmark(request, event_id: int):
         """
