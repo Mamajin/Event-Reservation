3312ca130880ec75ee3bb0c480618d4d2d5151dc
Sunthorn Kompita <sunthorn.ko@ku.th>
2024-11-23 12:11:02 +0700

    Create info completion page

--- frontend/src/components/EventDetail/sections/Comment.jsx
+++ frontend/src/components/EventDetail/sections/Comment.jsx
@@ -184,7 +184,7 @@ export function CommentSection({ event }) {
             : comment
         )
       );
-  
+      toggleReplies(replyingTo);
       setReplyContent('');
       setReplyingTo(null); // Reset replyingTo
     } catch (error) {
