f861910809bf3b62d18386faaf178e18d7bf6cb2
Sunthorn KOMPITA <sunthorn.ko@ku.th>
2024-11-30 04:41:54 +0700

    Add min and max date to date time input field

--- frontend/src/components/DateTimeInput.jsx
+++ frontend/src/components/DateTimeInput.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../style/index.css";
-
 function DateTimeInput({ label, name, value, onChange, required }) {
+
+    const currentYear = new Date().getFullYear();
+    const maxYear = currentYear + 100;
+    const minDate = `${currentYear}-01-01T00:00`;
+    const maxDate = `${maxYear}-12-31T23:59`;
+
     return (
         <div className="form-control w-full">
             <label className="label">
@@ -10,10 +15,12 @@ function DateTimeInput({ label, name, value, onChange, required }) {
             <input
                 type="datetime-local"
                 name={name}
-                value={value}
+                value={value || ""}
                 onChange={onChange}
                 className="input bg-gray-100 input-bordered w-full"
                 required={required}
+                min={minDate}
+                max={maxDate}  
             />
         </div>
     );
