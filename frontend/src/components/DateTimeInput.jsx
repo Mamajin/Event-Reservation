af0ae67f4a4d90c6ff8da268483ef0a5b798e60a
Sunthorn KOMPITA <sunthorn.ko@ku.th>
2024-11-30 04:57:17 +0700

    Add date formatter to make sure its show the old date time value

--- frontend/src/components/DateTimeInput.jsx
+++ frontend/src/components/DateTimeInput.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "../style/index.css";
 function DateTimeInput({ label, name, value, onChange, required }) {
-
+    const formatDateForInput = (dateTime) => {
+        const date = new Date(dateTime);
+        
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        return `${year}-${month}-${day}T${hours}:${minutes}`;
+      };
+          
     const currentYear = new Date().getFullYear();
     const maxYear = currentYear + 100;
     const minDate = `${currentYear}-01-01T00:00`;
@@ -15,13 +25,13 @@ function DateTimeInput({ label, name, value, onChange, required }) {
             <input
                 type="datetime-local"
                 name={name}
-                value={value || ""}
+                defaultValue={formatDateForInput(value) || ""}
                 onChange={onChange}
                 className="input bg-gray-100 input-bordered w-full"
                 required={required}
                 min={minDate}
-                max={maxDate}  
-            />
+                max={maxDate}
+                />
         </div>
     );
 }
