b9f623900bdbdb05ee9a394bc7bbbd5093dcfc1d
Phasit Ruangmak <phasit2005@gmail.com>
2024-12-01 14:05:54 +0700

    Add yml, badge, github aciton for CyPress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "r6s82d",
+
   e2e: {
     // Production URL https://event-reservation-isp.vercel.app/
     // Local Host URL http://localhost:5173/
@@ -14,6 +16,7 @@ export default defineConfig({
       openMode: 1
     }
   },
+  
   setupNodeEvents(on, config) {
     // implement node event listeners here
   },
