3c0fa4be8d30a565efc5f4fc083c4bf23c0db410
Sunthorn KOMPITA <sunthorn.ko@ku.th>
2024-12-02 17:42:55 +0700

    Update dependencies and tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,5 +12,5 @@ module.exports = {
     sans: ['Graphik', 'sans-serif'],
     serif: ['Merriweather', 'serif'],
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'),require('@tailwindcss/typography')],
 };
